8e98b28524b945c004d6cf82e435efa64f107130
Hiryu70 <che.maxv@gmail.com>
2020-01-05 12:27:52 +0300

    Initial

---
+++ GitTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitTestApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PrintHelloWorld("Hello World!");
+		}
+
+		static void PrintHelloWorld(string str)
+		{
+			Console.WriteLine(str);
+		}
+	}
+}
